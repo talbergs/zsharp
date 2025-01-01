4b4aedccf166cfce473d82f3adb4519829e28abd
Martins Talbergs <martins.talbergs@zabbix.com>
2025-01-01 13:04:26 +0200

    Add osx systems

--- flake.nix
+++ flake.nix
@@ -11,16 +11,23 @@
       ...
     }:
     let
+      map_supported_systems = function: nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darvin"
+        "aarch64-darvin"
+      ] (system: function nixpkgs.legacyPackages.${system});
+
       tool = ./tool;
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       layout_template = import ./layout_template.nix { inherit pkgs tool nixpkgs; };
       layout_generator = import ./layout_generator.nix { inherit pkgs layout_template; };
     in
     {
-
-      packages.x86_64-linux.default = import ./default.nix { inherit pkgs layout_generator; };
-      packages.x86_64-linux.layout_generator = layout_generator;
-      packages.x86_64-linux.layout_template = layout_template;
-
+      packages = map_supported_systems (pkgs: {
+        default = import ./default.nix { inherit pkgs layout_generator; };
+        layout_generator = layout_generator;
+        layout_template = layout_template;
+      });
     };
 }
