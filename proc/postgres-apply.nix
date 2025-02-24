feb3957385e4ef111b486ca0eb28e3e157432c14
Martins Talbergs <martins.talbergs@zabbix.com>
2025-02-24 13:30:54 +0200

    unified menu

--- proc/postgres-apply.nix
+++ proc/postgres-apply.nix
@@ -16,22 +16,29 @@ pkgs.mkShell {
   ];
   shellHook = ''
     source ${tool}/rtp.sh
-    echo "List: $(rtp:db_scheme '${session}')"
-    ${getExe pkgs.tree} "$(rtp:db_scheme '${session}')"
-
-    scheme="$(rtp:db_scheme '${session}')/postgresql.sql"
-
-    build() {
-      echo 'drop database "${dbname}";' | psql --host=0.0.0.0 --port=${dbport} --dbname=postgres
-      echo 'create database "${dbname}" encoding Unicode template template0;' | psql --host=0.0.0.0 --port=${dbport} --dbname=postgres
-      echo "\i $scheme;" | psql --host=0.0.0.0 --port=${dbport} --dbname=${dbname}
-    }
 
     fg() {
-      if read -r -p "RE-apply db dump?"
-      then
-        build
-      fi
+      clear
+      echo "List: $(rtp:db_scheme '${session}')"
+      ${getExe pkgs.tree} "$(rtp:db_scheme '${session}')"
+
+      printf "$(tput setaf 2)%s\n%s$(tput sgr0)\n\n" \
+        "1) RE-apply db dump?"
+      read -N 1 -e -p "[1]>:" var
+
+      case "$var" in
+        1)
+          scheme="$(rtp:db_scheme '${session}')/postgresql.sql"
+          echo 'drop database "${dbname}";' | psql --host=0.0.0.0 --port=${dbport} --dbname=postgres
+          echo 'create database "${dbname}" encoding Unicode template template0;' | psql --host=0.0.0.0 --port=${dbport} --dbname=postgres
+          echo "\i $scheme;" | psql --host=0.0.0.0 --port=${dbport} --dbname=${dbname}
+          fg
+        ;;
+        *)
+          echo "Choose an option."
+          fg
+        ;;
+      esac
     }
   '';
 }
