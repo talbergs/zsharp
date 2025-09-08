65f3302c613db66387c03410cda1faff98aeac8b
Martins Talbergs <martins.talbergs@zabbix.com>
2025-09-08 16:01:53 +0300

    x

--- tool/configs/zabbix.conf.php
+++ tool/configs/zabbix.conf.php
@@ -2,6 +2,7 @@
 
 $DB['TYPE']     = ZBX_DB_POSTGRESQL;
 $DB['SERVER']   = 'localhost';
-$DB['PORT']     = '{{ $PORT }}';
-$DB['DATABASE'] = '{{ $DATABASE }}';
-$DB['USER']     = '{{ $USER }}';
+$DB['PORT']     = 5433;
+$DB['DATABASE'] = ZABBIX_DB_VERSION;
+$DB['USER']     = 'postgres';
+$DB['PASSWORD'] = 'postgres';
