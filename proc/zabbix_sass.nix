005661de4b6afc83842ca893e21e75bbf03ed502
Martins Talbergs <martins.talbergs@zabbix.com>
2025-12-01 16:55:53 +0200

    ZBXNEXT-9920

--- proc/zabbix_sass.nix
+++ proc/zabbix_sass.nix
@@ -4,10 +4,28 @@ let
   builder = pkgs.writeShellScriptBin "builder" ''
     echo BUILDING STYLES in $PWD
     cmd="nix run ${tool}#sass --"
-    $cmd --no-cache --sourcemap=none sass/stylesheets/sass/screen.scss ui/assets/styles/blue-theme.css
-    $cmd --no-cache --sourcemap=none sass/stylesheets/sass/dark-theme.scss ui/assets/styles/dark-theme.css
-    $cmd --no-cache --sourcemap=none sass/stylesheets/sass/hc-light.scss ui/assets/styles/hc-light.css
+    # $cmd --no-cache --sourcemap=none sass/stylesheets/sass/screen.scss ui/assets/styles/blue-theme.css
+    # $cmd --no-cache --sourcemap=none sass/stylesheets/sass/dark-theme.scss ui/assets/styles/dark-theme.css
+    # $cmd --no-cache --sourcemap=none sass/stylesheets/sass/hc-light.scss ui/assets/styles/hc-light.css
+    # $cmd --no-cache --sourcemap=none sass/stylesheets/sass/hc-dark.scss ui/assets/styles/hc-dark.css
+
+    # cp sass/img/browser-sprite.png ui/assets/img/
+    # cp sass/apple-touch-icon-120x120-precomposed.png ui/assets/img/
+    # cp sass/apple-touch-icon-152x152-precomposed.png ui/assets/img/
+    # cp sass/apple-touch-icon-180x180-precomposed.png ui/assets/img/
+    # cp sass/apple-touch-icon-76x76-precomposed.png ui/assets/img/
+    # cp sass/ms-tile-144x144.png ui/assets/img/
+    # cp sass/touch-icon-192x192.png ui/assets/img/
+    # cp sass/favicon.ico ui/
+
     $cmd --no-cache --sourcemap=none sass/stylesheets/sass/hc-dark.scss ui/assets/styles/hc-dark.css
+    $cmd --no-cache --sourcemap=none sass/stylesheets/sass/hc-light.scss ui/assets/styles/hc-light.css
+    [ -e sass/stylesheets/sass/dark-classic-theme.scss ] && \
+    $cmd --no-cache --sourcemap=none sass/stylesheets/sass/dark-classic-theme.scss ui/assets/styles/dark-classic-theme.css
+    $cmd --no-cache --sourcemap=none sass/stylesheets/sass/dark-theme.scss ui/assets/styles/dark-theme.css
+    [ -e sass/stylesheets/sass/blue-classic-theme.scss ] && \
+    $cmd --no-cache --sourcemap=none sass/stylesheets/sass/blue-classic-theme.scss ui/assets/styles/blue-classic-theme.css
+    $cmd --no-cache --sourcemap=none sass/stylesheets/sass/blue-theme.scss ui/assets/styles/blue-theme.css
 
     cp sass/img/browser-sprite.png ui/assets/img/
     cp sass/apple-touch-icon-120x120-precomposed.png ui/assets/img/
@@ -17,6 +35,9 @@ let
     cp sass/ms-tile-144x144.png ui/assets/img/
     cp sass/touch-icon-192x192.png ui/assets/img/
     cp sass/favicon.ico ui/
+
+
+
   '';
 in
 with pkgs.lib;
--- tool/builders/zabbix_server.nix
+++ tool/builders/zabbix_server.nix
@@ -1,3 +1,16 @@
+# you could try something like:
+# export CFLAGS="-I/nix/store/b97gjl5zygaf6vzxsa1lf7jih8q4xi00-openssl-3.5.1-dev/include"
+# 
+# if it helps then certainly something is wrong on our side
+# 
+# or just specifly openssl include
+# 
+# I think we have problem with custom location that we need to check for include there
+# 
+# export CFLAGS="-I/nix/store/b97gjl5zygaf6vzxsa1lf7jih8q4xi00-openssl-3.5.1-dev/include"
+# or whatever is your incldue location
+
+
 { upstream ? <nixpkgs> }:
 let
   pkgs = import upstream { };
@@ -7,9 +20,10 @@ pkgs.callPackage ./make_zabbix_build_env.nix
   inherit pkgs;
   env = "server";
   configureFlags = [
+      # "--with-net-snmp"
       "--enable-server"
       "--with-postgresql"
       "--with-libcurl"
-      "--with-openssl=${pkgs.openssl.dev}"
+      # "--with-openssl=${pkgs.openssl.dev}"
   ];
 }
